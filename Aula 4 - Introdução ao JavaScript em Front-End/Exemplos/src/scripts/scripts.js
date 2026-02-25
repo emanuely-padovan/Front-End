0900fea53ee9c0675984ed16490f2c07a1b55887
emanuely-padovan <emanuely.padovan@aluno.senai.br>
2026-02-25 16:21:42 -0300

    FInalização da aula

--- Aula 4 - Introdução ao JavaScript em Front-End/Exemplos/src/scripts/scripts.js
+++ Aula 4 - Introdução ao JavaScript em Front-End/Exemplos/src/scripts/scripts.js
@@ -1,16 +1,49 @@
-const tempoAgora = new Date()
 
-const hora = tempoAgora.getHours()
+// const tempoAgora = new Date()
+// const hora = tempoAgora.getHours()
+// const dataFormatada = tempoAgora.toLocaleDateString('pt-BR')
+// const horaFormatada = tempoAgora.toLocaleTimeString('pt-BR')
 
-const dataFormatada = tempoAgora.toLocaleDateString('pt-BR')
-const horaFormatada = tempoAgora.toLocaleTimeString('pt-BR')
+// console.log('Hoje é dia: ' + dataFormatada)
+// console.log('Horário: ' + horaFormatada)
+// console.log('Só a hora: ' + hora)
 
-console.log('Hoje é dia: ' + dataFormatada)
-console.log('Horário: ' + horaFormatada)
-console.log('Só a hora: ' + hora)
+// if (hora > 12 && hora < 18) {
+//     alert ("Boa tarde!")
+// } else if (hora >= 18){
+//     alert ("Boa noite!")
+// } else {
+//     alert ("Bom dia!")
+// }
 
-if (hora > 12 && hora < 18) {
-    alert("Boa noite!")
-} else if (){
+// // DIA: 25/02/2026 - TECHFOOD
 
-}
+// class Prato {
+//     constructor(nome, preço){
+//         this.nome = nome
+//         this.preço = preço
+//     }
+//     exibirMoeda(resultado) {
+//         if(resultado) {
+//             return "R$" 
+//         } else {
+//             return "R$"
+//         }
+//     }
+// }
+
+// const lasanha = new Prato('Lasanha Bolonhesa', 45.00)
+// let qtd = prompt("Simulação: Quantas unidades de " + lasanha.nome + "você deseja?")
+
+// let total = lasanha.preço * qtd
+// alert ("Resumo da simulação: \n Prato: " + lasanha.nome + "\n Total: " + lasanha.exibirMoeda())
+
+
+
+
+alert("Bem vindo!")
+
+const nome = prompt("Para um atendimento mais personaizado. Digite seu nome: ")
+let nomeFormatado = nome.trim().toUpperCase()
+
+alert("Bem vindo "  + nomeFormatado + " ao restaurante Sabor & Saber")
---
+++ Aula 4 - Introdução ao JavaScript em Front-End/Exercícios/src/script/script.js
@@ -0,0 +1,70 @@
+// NOME: Emanuely Macedo Padovan
+// DATA: 25/02/2026
+
+// Exercício 1 - Personalizador de Acesso
+/*
+    const nome = prompt("Digite o seu NOME: ")
+    const sobrenome = prompt("Digite o seu SOBRENOME: ")
+
+    let nomeFormatado = nome.concat(`${sobrenome}`).trim()
+
+    alert("Olá, " + nomeFormatado.toUpperCase)
+    alert("Caracteres: " + nomeFormatado.length)
+*/
+
+// Exercício 2 - Calculadora de Divisão de Conta
+/*
+    const valor = prompt("Valor total da conta: ")
+    const quantidade = prompt("Quantas pessoas foram? ")
+
+    let div = valor/quantidade
+
+    alert("Cada amigo deve pagar R$ " + div.toFixed(2))
+*/
+
+// Exercício 3 - Validador de Promoção
+/*
+const valorCompra = prompt("Quanto foi gasto? ")
+const cupomResposta = prompt("Você possui algum tipo de cupom? (Apenas SIM ou NÃO) ").toLowerCase
+
+let cupom = false
+
+if (valorCompra > 150.00 || cupomResposta == "sim") {
+    cupom = true
+}
+
+if (cupom == true) {
+    alert("Frete grátis liberado!")
+} else {
+    alert("Frete pago!")
+}
+*/
+
+// Exercício 4 - Sorteador de Brindes
+/*
+const numero = prompt("Escolha um número de 1 a 10!")
+let numeroGerado = Math.floor(Math.random() * 10) + 1
+
+if (numero == numeroGerado) {
+    alert("Parabéns, você ganhou um brinde! O número sorteado foi " + numeroGerado)
+} else {
+    alert("Que pena, o número sorteado foi " + numeroGerado)
+}
+*/
+
+// Exercício 5 -Gestão de Frota
+
+const anoAtual = new Date()
+
+
+class Veiculo {
+    constructor(modelo, marca, ano) {
+        this.modelo = modelo
+        this.marca = marca
+        this.ano = ano
+    }
+    idadeVeiculo() {
+        let anoT = anoAtual.getFullYear()
+        const idade = this.ano - anoT
+    }
+}
